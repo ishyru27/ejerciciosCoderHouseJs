01f56885cfc36954ad6a701563b459e4a3b9c3c9
Luis Carvajal <39842662+ishyru27@users.noreply.github.com>
2022-07-13 20:21:13 -0300

    creation of script cycle with conditional

---
+++ scripts/cycle.js
@@ -0,0 +1,14 @@
+const valor1 = (dato1, dato2) =>{
+    let dato1 = num1;
+    let dato2 = num2;
+    let totalDatos = num1 + num2;
+    let cajaDato = 2;
+    console.log("El resultado es: " + totalDatos )
+}
+if(totalDatos){
+    for (let cajaDato = 0; cajaDato < totalDatos; cajaDato++) {
+       totalDatos = totalDatos+ cajaDato
+       console.log("total datos es igual a " + totalDatos)
+    }totalDatos = totalDatos- cajaDato
+    console.log("total datos en iguak "+ totalDatos)
+}
